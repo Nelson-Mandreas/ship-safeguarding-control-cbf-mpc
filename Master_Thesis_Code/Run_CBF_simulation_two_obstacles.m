5751e4432fb81b146ce076814bb283de05a4823f
Andreas Mork Nordsven <47330468+andras1234@users.noreply.github.com>
2025-12-05 11:33:54 +0100

    Update Run_CBF_simulation_two_obstacles.m

--- Master_Thesis_Code/Run_CBF_simulation_two_obstacles.m
+++ Master_Thesis_Code/Run_CBF_simulation_two_obstacles.m
@@ -555,5 +555,6 @@ legend('n_3','n_4','Location',legendLocation); grid on;
 subplot(3,1,3)
 plot(ts(1:end), rad2deg(uis(:,5)), 'LineWidth', 1.5); hold on;
 plot(ts(1:end), rad2deg(uis(:,6)), 'LineWidth', 1.5);
-xlabel('Time [s]'); ylabel('Azimuth angle [deg]');
+xlabel('Time [s]'); 
 legend('\alpha_1','\alpha_2','Location',legendLocation); title('Azimuth angles'); grid on;
+
